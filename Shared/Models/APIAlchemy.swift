f9e9e53664065d61a4f57520786dd0297324d1e0
Adrian Vatchinsky <avatchinsky@outlook.com>
2022-02-14 15:13:03 -0500

    Codified Alchemy Basic API

---
+++ Shared/Models/APIAlchemy.swift
@@ -0,0 +1,250 @@
+//
+//  APIAlchemy.swift
+//  EATWidget
+//
+//  Created by Adrian Vatchinsky on 2/14/22.
+//
+//  References:
+//      https://docs.alchemy.com/alchemy/enhanced-apis/nft-api/getnftmetadata
+//
+
+import Foundation
+
+// MARK: URI
+
+struct APIAlchemyUri {
+    let raw: URL?
+    let gateway: URL?
+}
+
+extension APIAlchemyUri: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case raw = "raw"
+        case gateway = "gateway"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        raw = (try? URL(string: container.decode(String.self, forKey: .raw))) ?? nil
+        gateway = (try? URL(string: container.decode(String.self, forKey: .gateway))) ?? nil
+    }
+}
+
+
+// MARK: Contract
+
+struct APIAlchemyContract {
+    let address: String
+}
+
+extension APIAlchemyContract: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case address = "address"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        address = try container.decode(String.self, forKey: .address)
+    }
+}
+
+// MARK: Token Metadata
+
+struct APIAlchemyTokenMetadata {
+    let tokenType: String
+}
+
+extension APIAlchemyTokenMetadata: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case tokenType = "tokenType"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        tokenType = try container.decode(String.self, forKey: .tokenType)
+    }
+}
+
+// MARK: Token Id
+
+struct APIAlchemyTokenId {
+    let tokenId: String
+    let tokenMetadata: APIAlchemyTokenMetadata
+}
+
+extension APIAlchemyTokenId: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case tokenId = "tokenId"
+        case tokenMetadata = "tokenMetadata"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        tokenId = try container.decode(String.self, forKey: .tokenId)
+        tokenMetadata = try container.decode(APIAlchemyTokenMetadata.self, forKey: .tokenMetadata)
+    }
+}
+
+// MARK: Attribute
+
+struct APIAlchemyAttribute {
+    let traitType: String
+    let value: String?
+}
+
+extension APIAlchemyAttribute: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case traitType = "trait_type"
+        case value = "value"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        traitType = try container.decode(String.self, forKey: .traitType)
+        
+        do {
+            value = try String(container.decode(Int.self, forKey: .value))
+        } catch DecodingError.typeMismatch {
+            value = try container.decode(String.self, forKey: .value)
+        }
+    }
+}
+
+// MARK: Media
+
+struct APIAlchemyMedia {
+    let uri: APIAlchemyUri
+}
+
+extension APIAlchemyMedia: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case uri = "uri"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        uri = try container.decode(APIAlchemyUri.self, forKey: .uri)
+    }
+}
+
+// MARK: Token Data
+
+struct APIAlchemyMetadata {
+    let title: String?
+    let text: String?
+    let imageUrl: URL?
+    let thumbnailUrl: URL?
+    let animationUrl: URL?
+    let externalUrl: URL?
+    let backgroundColor: String?
+    let attributes: [APIAlchemyAttribute]?
+}
+
+extension APIAlchemyMetadata: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case title = "name"
+        case text = "description"
+        case imageUrl = "image"
+        case thumbnailUrl = "thumbnail_url"
+        case animationUrl = "animation_url"
+        case externalUrl = "external_url"
+        case backgroundColor = "background_color"
+        case attributes = "attributes"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        title = (try? container.decode(String.self, forKey: .title)) ?? nil
+        text = (try? container.decode(String.self, forKey: .text)) ?? nil
+        
+        imageUrl = (try? URL(string: container.decode(String.self, forKey: .imageUrl))) ?? nil
+        
+        thumbnailUrl = (try? URL(string: container.decode(String.self, forKey: .thumbnailUrl))) ?? nil
+        
+        animationUrl = (try? URL(string: container.decode(String.self, forKey: .animationUrl))) ?? nil
+        
+        externalUrl = (try? URL(string: container.decode(String.self, forKey: .externalUrl))) ?? nil
+        
+        backgroundColor = (try? container.decode(String.self, forKey: .backgroundColor)) ?? nil
+        
+        attributes = (try? container.decode([APIAlchemyAttribute].self, forKey: .attributes)) ?? nil
+    }
+}
+
+
+// MARK: Token Data
+
+struct APIAlchemyNFT {
+    let contract: APIAlchemyContract
+    let id: APIAlchemyTokenId
+    
+    let title: String
+    let text: String
+    
+    let tokenUri: APIAlchemyUri
+    
+    let media: [APIAlchemyMedia]
+    let metadata: APIAlchemyMetadata?
+    
+    let timeLastUpdated: Date
+}
+
+extension APIAlchemyNFT: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case contract = "contract"
+        case id = "id"
+        case title = "title"
+        case text = "description"
+        case tokenUri = "tokenUri"
+        case media = "media"
+        case metadata = "metadata"
+        case timeLastUpdated = "timeLastUpdated"
+        
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        contract = try container.decode(APIAlchemyContract.self, forKey: .contract)
+        id = try container.decode(APIAlchemyTokenId.self, forKey: .id)
+        
+        title = try container.decode(String.self, forKey: .title)
+        text = try container.decode(String.self, forKey: .text)
+        
+        tokenUri = try container.decode(APIAlchemyUri.self, forKey: .tokenUri)
+        
+        media = try container.decode([APIAlchemyMedia].self, forKey: .media)
+        
+        metadata = (try? container.decode(APIAlchemyMetadata.self, forKey: .metadata)) ?? nil
+   
+        timeLastUpdated = try container.decode(Date.self, forKey: .timeLastUpdated)
+    }
+}
+
+// MARK: API Response
+
+struct APIAlchemyGetNFTsResponse {
+    let ownedNfts: [APIAlchemyNFT]
+    let totalCount: Int
+    let blockHash: String
+}
+
+extension APIAlchemyGetNFTsResponse: Decodable {
+    enum CodingKeys: String, CodingKey {
+        case ownedNfts = "ownedNfts"
+        case totalCount = "totalCount"
+        case blockHash = "blockHash"
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        ownedNfts = try container.decode([APIAlchemyNFT].self, forKey: .ownedNfts)
+        totalCount = try container.decode(Int.self, forKey: .totalCount)
+        blockHash = try container.decode(String.self, forKey: .blockHash)
+    }
+}
---
+++ Shared/Models/APIOpensea.swift
@@ -0,0 +1,8 @@
+//
+//  APIOpensea.swift
+//  EATWidget
+//
+//  Created by Adrian Vatchinsky on 2/14/22.
+//
+
+import Foundation
