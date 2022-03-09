2b0612e777224a742f26ba666098240a10b6aa46
Adrian Vatchinsky <avatchinsky@outlook.com>
2022-03-09 15:33:05 -0500

    copy

---
+++ EATWidget/Components/CollectionSection.swift
@@ -0,0 +1,20 @@
+//
+//  CollectionSection.swift
+//  EATWidget
+//
+//  Created by Adrian Vatchinsky on 3/9/22.
+//
+
+import SwiftUI
+
+struct CollectionSection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CollectionSection_Previews: PreviewProvider {
+    static var previews: some View {
+        CollectionSection()
+    }
+}
---
+++ EATWidget/ViewModels/CollectionSectionViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CollectionSectionViewModel.swift
+//  EATWidget
+//
+//  Created by Adrian Vatchinsky on 3/9/22.
+//
+
+import Foundation
