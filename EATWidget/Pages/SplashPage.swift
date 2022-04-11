01cfacf6c7b7ce6fb424d1240eff6848d6f86d8f
Adrian Vatchinsky <avatchinsky@outlook.com>
2022-04-11 11:00:48 -0400

    reset splash

--- EATWidget/Pages/SplashPage.swift
+++ EATWidget/Pages/SplashPage.swift
@@ -20,108 +20,108 @@ struct SplashPage: View {
     var body: some View {
         ZStack {
             
-            VStack {
-                Image(uiImage: UIImage(named: "eat-artifact-00")!)
-                    .resizable()
-                    .scaledToFit()
-            }
-            .padding()
+//            VStack {
+//                Image(uiImage: UIImage(named: "eat-artifact-00")!)
+//                    .resizable()
+//                    .scaledToFit()
+//            }
+//            .padding()
             
-//
-//            VStack(alignment: .leading) {
-//
-//                Spacer()
-//
-//                HStack {
-//                    Branding(seed: 0)
-//                        .frame(width: 42, height: 42)
-//
-//                    Spacer()
-//                }
-//                .padding(.horizontal, 10)
-//
-//                HStack {
-//                    if line1.isEmpty {
-//                        Spacer(minLength: 42)
-//                    } else {
-//                        Text(line1)
-//                            .font(.system(size: 42.0, design: .monospaced))
-//                            .fontWeight(.black)
-//                            .lineLimit(1)
-//                    }
-//
-//                    Spacer()
-//                }.padding(.horizontal)
-//
-//
-//                HStack {
-//                    if line2.isEmpty {
-//                        Spacer(minLength: 42)
-//                    } else {
-//                        Text(line2)
-//                            .font(.system(size: 42.0, design: .monospaced))
-//                            .fontWeight(.black)
-//                            .lineLimit(1)
-//                    }
-//
-//                    Spacer()
-//                }.padding(.horizontal)
-//
-//                HStack {
-//                    if line3.isEmpty {
-//                        Spacer(minLength: 42)
-//                    } else {
-//                        Text(line3)
-//                            .font(.system(size: 42.0, design: .monospaced))
-//                            .fontWeight(.black)
-//                            .lineLimit(1)
-//                    }
-//
-//                    Spacer()
-//                }.padding(.horizontal)
-//            }.padding(.bottom, safeAreaInsets.bottom)
+
+            VStack(alignment: .leading) {
+
+                Spacer()
+
+                HStack {
+                    Branding(seed: 0)
+                        .frame(width: 42, height: 42)
+
+                    Spacer()
+                }
+                .padding(.horizontal, 10)
+
+                HStack {
+                    if line1.isEmpty {
+                        Spacer(minLength: 42)
+                    } else {
+                        Text(line1)
+                            .font(.system(size: 42.0, design: .monospaced))
+                            .fontWeight(.black)
+                            .lineLimit(1)
+                    }
+
+                    Spacer()
+                }.padding(.horizontal)
+
+
+                HStack {
+                    if line2.isEmpty {
+                        Spacer(minLength: 42)
+                    } else {
+                        Text(line2)
+                            .font(.system(size: 42.0, design: .monospaced))
+                            .fontWeight(.black)
+                            .lineLimit(1)
+                    }
+
+                    Spacer()
+                }.padding(.horizontal)
+
+                HStack {
+                    if line3.isEmpty {
+                        Spacer(minLength: 42)
+                    } else {
+                        Text(line3)
+                            .font(.system(size: 42.0, design: .monospaced))
+                            .fontWeight(.black)
+                            .lineLimit(1)
+                    }
+
+                    Spacer()
+                }.padding(.horizontal)
+            }.padding(.bottom, safeAreaInsets.bottom)
         }
         .ignoresSafeArea()
         .onAppear {
-//            let l1 = "e.xperiments"
-//            let l2 = "a.rt"
-//            let l3 = "t.echnology_"
-//
-//            let delta = 0.1
-//
-//            l1.enumerated().forEach { index, character in
-//                DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * delta) {
-//
-//                  line1 += String(character)
-//
-//                }
-//            }
-//
-//            l2.enumerated().forEach { index, character in
-//                DispatchQueue.main.asyncAfter(deadline: .now() + (Double(l1.count) * delta) + Double(index) * delta) {
-//
-//                  line2 += String(character)
-//
-//                }
-//            }
-//
-//
-//            l3.enumerated().forEach { index, character in
-//                DispatchQueue.main.asyncAfter(deadline: .now() + (Double(l1.count) * delta) + (Double(l2.count) * delta) + Double(index) * delta) {
-//
-//                  line3 += String(character)
-//
-//                }
-//            }
-//
-//
-//            DispatchQueue.main.asyncAfter(deadline: .now() + (Double(l1.count) * delta) + (Double(l2.count) * delta) + (Double(l3.count) * delta) + 0.5) {
-//
-//                if onComplete != nil {
-//                    onComplete!()
-//                }
-//
-//            }
+            let l1 = "e.xperiments"
+            let l2 = "a.rt"
+            let l3 = "t.echnology_"
+
+            let delta = 0.1
+
+            l1.enumerated().forEach { index, character in
+                DispatchQueue.main.asyncAfter(deadline: .now() + Double(index) * delta) {
+
+                  line1 += String(character)
+
+                }
+            }
+
+            l2.enumerated().forEach { index, character in
+                DispatchQueue.main.asyncAfter(deadline: .now() + (Double(l1.count) * delta) + Double(index) * delta) {
+
+                  line2 += String(character)
+
+                }
+            }
+
+
+            l3.enumerated().forEach { index, character in
+                DispatchQueue.main.asyncAfter(deadline: .now() + (Double(l1.count) * delta) + (Double(l2.count) * delta) + Double(index) * delta) {
+
+                  line3 += String(character)
+
+                }
+            }
+
+
+            DispatchQueue.main.asyncAfter(deadline: .now() + (Double(l1.count) * delta) + (Double(l2.count) * delta) + (Double(l3.count) * delta) + 0.5) {
+
+                if onComplete != nil {
+                    onComplete!()
+                }
+
+            }
             
             
         }
